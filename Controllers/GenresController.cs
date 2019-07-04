d740fd117f907dd9f52187f939e3443517b4c572
KhanhTranPC <khanhtrant12@outlook.com>
2019-07-05 00:24:00 +0700

    first commit

---
+++ Controllers/GenresController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MISA.MovieStore.Data;
+using MISA.MovieStore.Models;
+
+namespace MISA.MovieStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GenresController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Genres
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Genre>>> GetGenres()
+        {
+            return await _context.Genres.ToListAsync();
+        }
+
+        // GET: api/Genres/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Genre>> GetGenre(byte id)
+        {
+            var genre = await _context.Genres.FindAsync(id);
+
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            return genre;
+        }
+
+        // PUT: api/Genres/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGenre(byte id, Genre genre)
+        {
+            if (id != genre.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(genre).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GenreExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Genres
+        [HttpPost]
+        public async Task<ActionResult<Genre>> PostGenre(Genre genre)
+        {
+            _context.Genres.Add(genre);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (GenreExists(genre.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetGenre", new { id = genre.Id }, genre);
+        }
+
+        // DELETE: api/Genres/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Genre>> DeleteGenre(byte id)
+        {
+            var genre = await _context.Genres.FindAsync(id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            _context.Genres.Remove(genre);
+            await _context.SaveChangesAsync();
+
+            return genre;
+        }
+
+        private bool GenreExists(byte id)
+        {
+            return _context.Genres.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MISA.MovieStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MISA.MovieStore.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
