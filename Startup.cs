e75b4da006861667f1fea63b01a804bf755a90f9
KhanhTranPC <khanhtrant12@outlook.com>
2019-07-05 23:12:46 +0700

    Update docker-compose

--- Startup.cs
+++ Startup.cs
@@ -27,8 +27,7 @@ namespace MISA.MovieStore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var connection = @"Server=sql.data;Database=MISA.MovieStore;User=sa;Password=Your_password123;";
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connection));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddSwaggerGen(c =>
